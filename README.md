dd4009ddf058d4ad075ba5b271aad54d22b46ee0
phl3bas <phl3bas87@gmail.com>
2021-02-11 15:11:26 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Snippt.dev
+
+Instructions for Developers
+
+```bash
+# in root directory!
+npm install
+
+# start server (will run on port 8000)
+npm run start:api
+
+# start frontend (will run on port 3000)
+npm run start:client
+```
---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Hello World</h1>
+        <hr />
+        NextJS
+      </div>
+    </div>
+  );
+}
