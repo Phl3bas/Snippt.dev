5a9eec66b62eb03e5c8ea4556aadfb220a23b060
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2021-03-24 14:44:29 +0000

    api - add base user impl

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/appconfig.module';
 import { SnippetModule } from './snippet/snippet.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AppConfigModule, SnippetModule],
+  imports: [AppConfigModule, SnippetModule, UserModule],
 })
 export class AppModule {}
